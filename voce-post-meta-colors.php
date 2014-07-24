c9ce7b438df4bcb968bf63f0643ad082b4e27523
Curtis Loisel <curtis@voceconnect.com>
2014-07-24 16:56:51 -0400

    Adding ability to pass palettes array into args

--- voce-post-meta-colors.php
+++ voce-post-meta-colors.php
@@ -133,10 +133,11 @@ Voce_Post_Meta_Colors::initialize();
  * @param int $post_id
  */
 function voce_color_field_display( $field, $value, $post_id ) {
+	$palettes = isset($field->args['palettes']) ? $field->args['palettes'] : true;
 	?>
 	<p>
 		<?php voce_field_label_display( $field ); ?>
-		<input type="text" class="voce-color-picker" id="<?php echo $field->get_input_id( ); ?>" name="<?php echo $field->get_name( ); ?>" value="<?php echo esc_attr( $value ); ?>" />
+		<input data-palettes="<?php echo esc_attr(json_encode($palettes)); ?>" type="text" class="voce-color-picker" id="<?php echo $field->get_input_id( ); ?>" name="<?php echo $field->get_name( ); ?>" value="<?php echo esc_attr( $value ); ?>" />
 	</p>
 	<?php
 }
